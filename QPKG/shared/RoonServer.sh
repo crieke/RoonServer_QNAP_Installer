2ef6c4b7756f190dbf336d92a827efc5b314adfc
Christopher Rieke <github@mail.rieke.tv>
2016-06-06 17:49:17 +0200

    cleaned up script

--- QPKG/shared/RoonServer.sh
+++ QPKG/shared/RoonServer.sh
@@ -5,10 +5,10 @@ QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
 ROON_LIB_DIR="${QPKG_ROOT}/lib64"
 ROON_TMP_DIR="${QPKG_ROOT}/tmp"
 ROON_PIDFILE="${QPKG_ROOT}/RoonServer.pid"
-ROON_DATAROOT=`( /sbin/getcfg $QPKG_NAME path -f /etc/config/smb.conf )`
+ROON_DATAROOT=`/sbin/getcfg $QPKG_NAME path -f /etc/config/smb.conf`
 
 if [ -f $ROON_PIDFILE ]; then
-    PID=`( cat "${ROON_PIDFILE}" )`
+    PID=`cat "${ROON_PIDFILE}"`
 fi
 
 start_daemon ()
@@ -16,14 +16,14 @@ start_daemon ()
         #Launch the service in the background if RoonServer share exists.
         if [ "${ROON_DATAROOT}" != "" ]; then
             export ROON_DATAROOT="$ROON_DATAROOT"
-            export LD_LIBRARY_PATH="${ROON_LIB_DIR}"
+            export LD_LIBRARY_PATH="${ROON_LIB_DIR}:${LD_LIBRARY_PATH}"
             export ROON_INSTALL_TMPDIR="${ROON_TMP_DIR}"
             ${QPKG_ROOT}/RoonServer/start.sh &
             echo $! > "${ROON_PIDFILE}"
             /sbin/write_log "[RoonServer] ROON_UPDATE_TMP_DIR = ${ROON_TMP_DIR}" 4
             /sbin/write_log "[RoonServer] ROON_DATAROOT = ${ROON_DATAROOT}" 4
             /sbin/write_log "[RoonServer] Additional library folder = ${ROON_LIB_DIR}" 4
-            /sbin/write_log "[RoonServer] PID = `( cat ${ROON_PIDFILE} )`" 4
+            /sbin/write_log "[RoonServer] PID = `cat ${ROON_PIDFILE}`" 4
         else
             /sbin/write_log "[RoonServer] Shared folder \"RoonServer\" could not be found. Please create it in the QTS before launching the package." 1
         fi
